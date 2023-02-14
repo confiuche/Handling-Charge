9783f49dd246241486f5287e67dbde616a18edc2
Irechukwu Confidence <irechukwuconfidence@mail.com>
2023-02-14 16:49:14 +0100

    .

--- js/handling.js
+++ js/handling.js
@@ -9,41 +9,96 @@
     }
 }*/
 
-function getBill(){
-    let awbWt = document.getElementById("wt").value;
+// document.getElementById("btnBill").addEventListener("click",bill)
 
-    let amt = awbWt * 65.3;
-    //let dem = awbWt * 13 * days
-    // let demVat = dem * 0.075;
-    let vat = amt * 0.075;
-    let anlca = awbWt * 3;
-    let total = amt + vat + anlca;
+
+// function general(){
+//     function bill(){
+//     let awbWt = document.getElementById("wt").value;
+
+//     let amt = awbWt * 65.3;
+//     //let dem = awbWt * 13 * days
+//     // let demVat = dem * 0.075;
+//     let vat = amt * 0.075;
+//     let anlca = awbWt * 3;
+//     let total = amt + vat + anlca;
     
-    // let height = document.getElementById("height").value;
-    // let area = (base * height)/2;
-   // console.log(area);
+//     // let height = document.getElementById("height").value;
+//     // let area = (base * height)/2;
+//    // console.log(area);
 
-   document.getElementById("amt").innerText = `Amount: ${amt}`
-   document.getElementById("vat").innerText = `Vat7.5%: ${vat}`
-   document.getElementById("anlca").innerText = `Anlca: ${anlca}`
-   document.getElementById("bill").innerText = `Total Amount: ${total}`
+//    document.getElementById("amt").innerText = `Amount: ${amt}`;
+//    document.getElementById("vat").innerText = `Vat7.5%: ${vat}`;
+//    document.getElementById("anlca").innerText = `Anlca: ${anlca}`;
+//    document.getElementById("bill").innerText = `Total Amount: ${total}`;
+// }
+// }
+
+// // Dangerous Item
+// function dangerous(){
+//     let awbWt = document.getElementById("wt").value;
+
+//     let amt = awbWt * 82;
+//     let vat = amt * 0.075;
+//     let anlca = awbWt * 3;
+//     let total = amt + vat + anlca;
+    
+//     // let height = document.getElementById("height").value;
+//     // let area = (base * height)/2;
+//    // console.log(area);
+
+//    document.getElementById("amt").innerText = `Amount: ${amt}`;
+//    document.getElementById("vat").innerText = `Vat7.5%: ${vat}`;
+//    document.getElementById("anlca").innerText = `Anlca: ${anlca}`;
+//    document.getElementById("bill").innerText = `Total Amount: ${total}`;
+// }
+
+
+// let charges = " ";
+// do{
+//     let input = prompt("Select from menu:\n1: General Cargo \n2: Dangerous Goods \n Exit")
+//     if(input === "1"){
+//         general();
+//     }else if(input === "2"){
+//         dangerous()
+//     }else{
+//         alert("Good Bye");
+//     }
+//     charges = prompt("Do you want to perform another transaction? y/n");
+// }while(charges !== "n");
+
+
+
+function general(){
+    weight = parseInt(prompt("Pls, Enter the weight of your shipment"));
+    var amount = weight * 65.3;
+    var vat7 = amount * 0.075;
+    var anclaChg = weight * 3;
+    total = amount + vat7 + anclaChg;
+    alert("***** INVOICE CHARGE ***** \n" + "Handling Charge For "+ weight+"kg" + "\n Handling Charge: " 
+    + amount  + "\n Vat7.5%: " + vat7  + "\n Ancla: " + anclaChg + "\n Total Amount: " + total);
 }
 
-// Dabgerous Item
-function getBill1(){
-    let awbWt = document.getElementById("wt").value;
-
-    let amt = awbWt * 82;
-    let vat = amt * 0.075;
-    let anlca = awbWt * 3;
-    let total = amt + vat + anlca;
-    
-    // let height = document.getElementById("height").value;
-    // let area = (base * height)/2;
-   // console.log(area);
-
-   document.getElementById("amt1").innerText = `Amount: ${amt}`
-   document.getElementById("vat1").innerText = `Vat7.5%: ${vat}`
-   document.getElementById("anlca1").innerText = `Anlca: ${anlca}`
-   document.getElementById("bill1").innerText = `Total Amount: ${total}`
+function dangerous(){
+    weight = parseInt(prompt("Pls, Enter the weight of your shipment"));
+    var amount = weight * 82;
+    var vat7 = amount * 0.075;
+    var anclaChg = weight * 3;
+    total = amount + vat7 + anclaChg;
+    alert("***** INVOICE CHARGE ***** \n" + "Handling Charge For "+ weight+"kg" + "\n Handling Charge: " 
+    + amount + "\n Vat7.5%: " + vat7 +"\n Ancla: " + anclaChg + "\n Total Amount: " + total);
 }
+
+
+let charges = " ";
+do{
+    let input = prompt("Select from menu:\n1: General Cargo \n2: Dangerous Goods \n Exit")
+    if(input === "1"){
+        general();
+    }else if(input === "2"){
+        dangerous()
+    }else{
+        alert("Good Bye");
+    }
+    charges = prompt("Do you want to perform another transaction? y/n");
+}while(charges !== "n");
